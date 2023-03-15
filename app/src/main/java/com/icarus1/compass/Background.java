a5cd6dde2f3a8e1557f6352481b86070a4f1d890
Simon Scott <simon.t.scott@outlook.com>
2023-03-15 16:58:18 +0000

    Color palette changed.

--- app/src/main/java/com/icarus1/compass/Background.java
+++ app/src/main/java/com/icarus1/compass/Background.java
@@ -3,6 +3,7 @@ package com.icarus1.compass;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
 import com.icarus1.R;
@@ -21,6 +22,7 @@ class Background {
     private final Paint altitudeTrackPaint;
     private final Paint altitudeTextPaint;
     private final Paint NSEWPaint;
+    private final int NColor;
 
     public Background(Context context, float radius) {
 
@@ -40,8 +42,11 @@ class Background {
         altitudeTextPaint.setStyle(Paint.Style.FILL);
 
         NSEWPaint = new Paint();
-        NSEWPaint.setColor(resources.getColor(R.color.compass_NSEW, theme));
+        NSEWPaint.setColor(resources.getColor(R.color.compass_SEW, theme));
         NSEWPaint.setStyle(Paint.Style.FILL);
+        NSEWPaint.setFakeBoldText(true);
+
+        NColor = resources.getColor(R.color.compass_N, theme);
 
         setRadius(radius);
 
@@ -68,10 +73,15 @@ class Background {
 
     public void draw(Canvas canvas) {
 
-        float textSize = NSEWPaint.getTextSize();
-        float padding = textSize / 4f;
-
         canvas.drawOval(0, 0, diameter, diameter, backgroundPaint);
+
+        drawAltitudeTracks(canvas);
+        drawNESW(canvas);
+
+    }
+
+    private void drawAltitudeTracks(Canvas canvas) {
+
         for (int i = 0; i < 8; i++) {
 
             float trackRadius = (8-i)* radius /9f;
@@ -80,43 +90,56 @@ class Background {
 
             String text = 10*(i+1) + "\u00B0";
             canvas.drawText(
-                text,
-                radius - altitudeTextPaint.measureText(text)/2,
-                radius - trackRadius - altitudeTextPaint.getStrokeWidth(),
-                altitudeTextPaint
+                    text,
+                    radius - altitudeTextPaint.measureText(text)/2,
+                    radius - trackRadius - altitudeTextPaint.getStrokeWidth(),
+                    altitudeTextPaint
             );
 
         }
         canvas.drawCircle(radius, radius, 1, altitudeTrackPaint);
+
+    }
+
+    private void drawNESW(Canvas canvas) {
+
+        float textSize = NSEWPaint.getTextSize();
+        float padding = textSize / 4f;
 
         float sizeN = NSEWPaint.measureText(TEXT_N);
         float sizeS = NSEWPaint.measureText(TEXT_S);
         float sizeE = NSEWPaint.measureText(TEXT_E);
 
         canvas.drawText(
-            TEXT_N,
-            radius - sizeN / 2f,
-            padding + textSize,
-            NSEWPaint
+                TEXT_S,
+                radius - sizeS / 2f,
+                diameter - padding,
+                NSEWPaint
         );
         canvas.drawText(
-            TEXT_S,
-            radius - sizeS / 2f,
-            diameter - padding,
-            NSEWPaint
+                TEXT_E,
+                diameter - padding - sizeE,
+                radius + textSize / 2f,
+                NSEWPaint
         );
         canvas.drawText(
-            TEXT_E,
-            diameter - padding - sizeE,
-            radius + textSize / 2f,
-            NSEWPaint
+                TEXT_W,
+                padding,
+                radius + textSize / 2f,
+                NSEWPaint
         );
+
+        int SEWColor = NSEWPaint.getColor();
+        NSEWPaint.setColor(NColor);
+
         canvas.drawText(
-            TEXT_W,
-            padding,
-            radius + textSize / 2f,
-            NSEWPaint
+                TEXT_N,
+                radius - sizeN / 2f,
+                padding + textSize,
+                NSEWPaint
         );
+
+        NSEWPaint.setColor(SEWColor);
 
     }
 
