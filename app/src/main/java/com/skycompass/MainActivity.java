88b384534d8bcf8b190798b6f17a9badbba938e9
Simon Scott <simon.t.scott@outlook.com>
2024-08-27 19:45:39 +0100

    Navigation and preferences.

--- app/src/main/java/com/skycompass/MainActivity.java
+++ app/src/main/java/com/skycompass/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skycompass;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.MenuProvider;
@@ -10,7 +11,9 @@ import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,86 +21,62 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.skycompass.databinding.ActivityMainBinding;
-import com.skycompass.settings.SavedSettings;
 import com.skycompass.util.Debug;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private static final SavedSettings.NightModeListener NIGHT_MODE_LISTENER = new NightModeListener();
-
-    private SavedSettings savedState;
+    private SharedPreferences preferences;
 
     private ActivityMainBinding binding;
+
     private NavController navigationController;
+
     private final MenuListener menuListener = new MenuListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SavedSettings.NightMode currentNightMode;
-        int currentNightModeCode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        if (currentNightModeCode == Configuration.UI_MODE_NIGHT_YES) {
-            currentNightMode = SavedSettings.NightMode.NIGHT;
-        } else if (currentNightModeCode == Configuration.UI_MODE_NIGHT_NO) {
-            currentNightMode = SavedSettings.NightMode.DAY;
-        } else {
-            currentNightMode = null;
-        }
+        // Preferences
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        savedState = SavedSettings.from(this);
-        savedState.setNightModeListener(NIGHT_MODE_LISTENER);
+        preferences.registerOnSharedPreferenceChangeListener(this);
 
-        SavedSettings.NightMode savedNightMode = savedState.getNightMode();
+        //
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        boolean requiresUpdate = false;
+
+        String savedNightMode = preferences.getString("NightMode", "");
+
         switch (savedNightMode) {
-            case DAY :
+            case "DAY" :
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                requiresUpdate = (currentNightMode & Configuration.UI_MODE_NIGHT_YES) != 0;
                 break;
-            case NIGHT :
+            case "NIGHT" :
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                requiresUpdate = (currentNightMode & Configuration.UI_MODE_NIGHT_NO) != 0;
                 break;
             default :
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                break;
         }
 
-        if (
-            savedNightMode != SavedSettings.NightMode.SYSTEM &&
-            currentNightMode != null &&
-            currentNightMode != savedNightMode
-        ) {
+        if (requiresUpdate)
             return;
-        }
 
-        initBindingAndContentView();
-        initToolbarAndActionBar();
-
-        addMenuProvider(menuListener);
-
-    }
-
-    @Override
-    protected void onDestroy() {
-
-        savedState.destroy();
-
-        super.onDestroy();
-    }
-
-    private void initBindingAndContentView() {
-
+        // Inflate views
         binding = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
+
         setContentView(binding.getRoot());
 
-    }
-
-    private void initToolbarAndActionBar() {
-
+        // Create fragment
         Fragment mainFragmentContainer = getSupportFragmentManager().findFragmentById(R.id.main_fragment_container);
 
         if (mainFragmentContainer == null)
             throw new RuntimeException("Main fragment not found.");
 
+        // Initialise app bar and navigation
         NavHostFragment mainNavHostFragment = (NavHostFragment) mainFragmentContainer;
 
         navigationController = mainNavHostFragment.getNavController();
@@ -106,6 +85,16 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.toolbar, navigationController, appBarConfiguration);
         setSupportActionBar(binding.toolbar);
 
+        addMenuProvider(menuListener);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+
+        super.onDestroy();
     }
 
     private class MenuListener implements MenuProvider {
@@ -120,14 +109,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPrepareMenu(@NonNull Menu menu) {
 
-            try {
-                NavDestination dest = navigationController.getCurrentDestination();
-                if (dest != null) {
-                    currentDestination = dest.getId();
-                }
-            } catch (NullPointerException e){
-                Debug.error(e.getMessage());
-            }
+            NavDestination dest = navigationController.getCurrentDestination();
+
+            if (dest != null)
+                currentDestination = dest.getId();
 
             menu.setGroupVisible(R.id.menu_group_core, true);
             menu.findItem(R.id.menu_item_dark_mode).setVisible(currentDestination == R.id.navigation_main_fragment_main);
@@ -135,19 +120,22 @@ public class MainActivity extends AppCompatActivity {
             menu.findItem(R.id.menu_item_settings).setVisible(currentDestination == R.id.navigation_main_fragment_main);
 
             MenuItem darkModeItem = menu.findItem(R.id.menu_item_dark_mode);
+
             int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
 
-            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
-                darkModeItem.setIcon(R.drawable.light_mode);
-                darkModeItem.setTitle(R.string.light_mode);
-            } else if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO) {
-                darkModeItem.setIcon(R.drawable.dark_mode);
-                darkModeItem.setTitle(R.string.dark_mode);
-            } else if (nightModeFlags == Configuration.UI_MODE_NIGHT_UNDEFINED) {
-                darkModeItem.setIcon(R.drawable.dark_mode);
-                darkModeItem.setTitle(R.string.dark_mode);
-            } else {
-                Debug.log("Unrecognised nightModeFlags.");
+            switch (nightModeFlags) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    darkModeItem.setIcon(R.drawable.light_mode);
+                    darkModeItem.setTitle(R.string.light_mode);
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                    darkModeItem.setIcon(R.drawable.dark_mode);
+                    darkModeItem.setTitle(R.string.dark_mode);
+                    break;
+                default:
+                    Debug.warn("Night mode not recognised.");
+                    darkModeItem.setIcon(R.drawable.dark_mode);
+                    darkModeItem.setTitle(R.string.dark_mode);
             }
 
         }
@@ -158,30 +146,36 @@ public class MainActivity extends AppCompatActivity {
             int id = menuItem.getItemId();
 
             if (id == R.id.menu_item_settings) {
+
                 if(currentDestination == R.id.navigation_main_fragment_main) {
                     navigationController.navigate(R.id.navigation_action_main_to_settings);
                     invalidateMenu();
                 }
-                return true;
-            }
 
-            if (id == R.id.menu_item_help) {
+                return true;
+
+            } else if (id == R.id.menu_item_help) {
+
                 if (currentDestination == R.id.navigation_main_fragment_main) {
                     navigationController.navigate(R.id.navigation_action_main_to_help);
                     invalidateMenu();
                 }
-                return true;
-            }
 
-            if (id == R.id.menu_item_dark_mode) {
+                return true;
+
+            } else if (id == R.id.menu_item_dark_mode) {
+
                 toggleNightMode();
-                return true;
-            }
 
-            if (id == android.R.id.home) {
+                return true;
+
+            } else if (id == android.R.id.home) {
+
                 navigationController.navigateUp();
                 invalidateMenu();
+
                 return true;
+
             }
 
             return false;
@@ -193,35 +187,45 @@ public class MainActivity extends AppCompatActivity {
 
         int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
 
-        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
-            savedState.stageNightMode(SavedSettings.NightMode.DAY);
-        } else if (currentNightMode == Configuration.UI_MODE_NIGHT_NO) {
-            savedState.stageNightMode(SavedSettings.NightMode.NIGHT);
-        } else if (currentNightMode == Configuration.UI_MODE_NIGHT_UNDEFINED) {
-            savedState.stageNightMode(SavedSettings.NightMode.DAY);
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                preferences.edit()
+                        .putString("NightMode", "DAY")
+                        .apply();
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                preferences.edit()
+                        .putString("NightMode", "NIGHT")
+                        .apply();
+                break;
+            default:
+                Debug.warn("Night mode not recognised.");
+                preferences.edit()
+                        .putString("NightMode", "DAY")
+                        .apply();
+                break;
         }
-
-        savedState.apply();
 
     }
 
-    private static class NightModeListener implements SavedSettings.NightModeListener {
-        @Override
-        public void onNightModeChanged(SavedSettings.NightMode nightMode) {
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String s) {
 
-            switch (nightMode) {
-                case DAY :
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    break;
-                case NIGHT :
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    break;
-                default :
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-                    break;
-            }
+        if (s == null || !s.equals("NightMode"))
+            return;
 
+        switch (sharedPreferences.getString("NightMode", "")) {
+            case "DAY" :
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case "NIGHT" :
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            default :
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
         }
+
     }
 
 }
--- app/src/main/java/com/skycompass/PreferenceFragment.java
+++ app/src/main/java/com/skycompass/PreferenceFragment.java
@@ -1,23 +1,12 @@
-package com.skycompass.settings;
+package com.skycompass;
 
 import android.content.res.Configuration;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.view.MenuProvider;
 import androidx.preference.PreferenceFragmentCompat;
-
-import com.skycompass.R;
-import com.skycompass.util.Debug;
 
 public class PreferenceFragment extends PreferenceFragmentCompat {
 
@@ -25,11 +14,10 @@ public class PreferenceFragment extends PreferenceFragmentCompat {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        if ((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES) {
+        if ((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES)
             view.setBackgroundColor(requireContext().getColor(R.color.grey_dark));
-        } else {
+        else
             view.setBackgroundColor(requireContext().getColor(R.color.cream_light));
-        }
 
     }
 
@@ -37,5 +25,4 @@ public class PreferenceFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
     }
-
 }
