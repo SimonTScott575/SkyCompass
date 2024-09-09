1bd6c7edc4e86e53f6ecdc33dad6275c2d35cdb3
Simon Scott <simon.t.scott@outlook.com>
2024-09-09 17:33:20 +0100

    Material 3 theme colours.

--- app/src/main/java/com/skycompass/PreferenceFragment.java
+++ app/src/main/java/com/skycompass/PreferenceFragment.java
@@ -11,17 +11,6 @@ import androidx.preference.PreferenceFragmentCompat;
 public class PreferenceFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        if ((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES)
-            view.setBackgroundColor(requireContext().getColor(R.color.grey_dark));
-        else
-            view.setBackgroundColor(requireContext().getColor(R.color.cream_light));
-
-    }
-
-    @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
     }
